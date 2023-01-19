ad5be44edeb66eb52bb5e345375560b89836172e
ayugcha <alexgoth1992@hotmail.com>
2023-01-19 14:30:20 -0500

    LAB2.2_NUM_PRIMO1

--- SEMANA 2/Numero _primo.py
+++ SEMANA 2/Numero _primo.py
@@ -1,1 +1,11 @@
+num=int(input("Ingresa un numero"))
+primo=True
 
+for n in  range(2,num):
+    if num % n == 0:
+        print("no es primo",n,"es divisor")
+        primo=False
+        break
+
+if primo:
+    print("Es primo")
